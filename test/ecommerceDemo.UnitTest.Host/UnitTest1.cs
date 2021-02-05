07ed51a032991285de98a1c4f92fd306e7af9294
onurkybsi <onurbpm@outlook.com>
2021-02-05 16:03:39 +0300

    First commit

---
+++ test/ecommerceDemo.UnitTest.Host/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ecommerceDemo.UnitTest.Host
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
