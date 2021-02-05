b397790e28e997ae14f3ee484f550a05846c5112
onurkybsi <onurbpm@outlook.com>
2021-02-05 16:27:51 +0300

    ELKLogger added

--- src/ecommerceDemo.Host/Program.cs
+++ src/ecommerceDemo.Host/Program.cs
@@ -1,5 +1,11 @@
+using System;
+using System.IO;
+using Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ecommerceDemo.Host
 {
@@ -7,14 +13,26 @@ namespace ecommerceDemo.Host
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = InitialHelper.GetConfiguration(Directory.GetCurrentDirectory(), Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+
+            Log.Logger = InitialHelper.CreateELKLogger(new ELKLoggerSettings
+            {
+                AppName = configuration["AppName"],
+                ElasticsearchURL = configuration["ElasticsearchURL"]
+            });
+
+            var host = CreateHostBuilder(args, configuration).Build();
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration)
+            => Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(x => x.AddConfiguration(configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseKestrel(options => options.AddServerHeader = false);
                     webBuilder.UseStartup<Startup>();
-                });
+                }).ConfigureLogging(config => config.ClearProviders()).UseSerilog();
     }
 }
--- src/ecommerceDemo.Host/Startup.cs
+++ src/ecommerceDemo.Host/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ecommerceDemo.Host
 {
@@ -22,21 +15,17 @@ namespace ecommerceDemo.Host
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
+            else
+                app.UseHttpsRedirection();
 
             app.UseRouting();
 
@@ -46,6 +35,8 @@ namespace ecommerceDemo.Host
             {
                 endpoints.MapControllers();
             });
+
+            Serilog.Log.ForContext<Startup>().Information("{Application} is listening on {Env}...", Configuration["AppName"], env.EnvironmentName);
         }
     }
 }
