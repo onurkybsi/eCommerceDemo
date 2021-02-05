e26758c622b48d72021fc976a4710a0f205c94fc
onurkybsi <onurbpm@outlook.com>
2021-02-05 21:49:03 +0300

    FakeDataGenerator added

---
+++ src/ecommerceDemo.Data/Utility/FakeDataGenerator.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using ecommerceDemo.Data.Model;
+
+namespace ecommerce.Data.Utility
+{
+    public static class FakeDataGenerator 
+    {
+        // TO-DO
+    }
+}
